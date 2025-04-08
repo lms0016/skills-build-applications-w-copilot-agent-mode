c85f54dcd6bccccd6a12b012b8a98232d846fbfe
Alan Lin <lms001616@gmail.com>
2025-04-08 10:24:18 +0000

    Update styles in App.css and prepare for deployment

---
+++ octofit-tracker/frontend/src/components/Leaderboard.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+function Leaderboard() {
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    fetch('https://crispy-disco-pj6x4gp749hrq55-8000.app.github.dev/api/leaderboard')
+      .then(response => response.json())
+      .then(data => setLeaderboard(data));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="my-4">Leaderboard</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard.map(entry => (
+            <tr key={entry.id}>
+              <td>{entry.name}</td>
+              <td>{entry.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Leaderboard;
